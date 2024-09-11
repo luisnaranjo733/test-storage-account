fcfc34c9a8a42f68e93bac02d031f72a2ae1b52e
Luis Naranjo <jonaranj@microsoft.com>
2024-09-11 11:35:57 -0700

    add test script

---
+++ index.mjs
@@ -0,0 +1,47 @@
+import { DefaultAzureCredential } from "@azure/identity"
+import { BlobServiceClient} from "@azure/storage-blob"
+
+const account = "salppcdevtede2cz6plihq";
+const defaultAzureCredential = new DefaultAzureCredential();
+
+const blobServiceClient = new BlobServiceClient(
+  `https://${account}.blob.core.windows.net`,
+  defaultAzureCredential
+);
+
+const containerName = "testcontainer";
+const blobName = "package.json";
+
+async function main() {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+    const blobClient = containerClient.getBlobClient(blobName);
+  
+    // Get blob content from position 0 to the end
+    // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+    const downloadBlockBlobResponse = await blobClient.download();
+    const downloaded = (
+      await streamToBuffer(downloadBlockBlobResponse.readableStreamBody)
+    ).toString();
+    console.log("Downloaded blob content:", downloaded);
+  
+    // [Node.js only] A helper method used to read a Node.js readable stream into a Buffer
+    async function streamToBuffer(readableStream) {
+      return new Promise((resolve, reject) => {
+        const chunks = [];
+        readableStream.on("data", (data) => {
+          chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+        });
+        readableStream.on("end", () => {
+          resolve(Buffer.concat(chunks));
+        });
+        readableStream.on("error", reject);
+      });
+    }
+  }
+  
+try {
+    main()
+} catch (error) {
+    console.error("Unhandled exception from main")
+    console.error(error)
+}
